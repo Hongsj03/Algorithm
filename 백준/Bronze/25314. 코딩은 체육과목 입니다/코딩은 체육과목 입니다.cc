f68b54afc2ca0d6c900b896d7419860d0de7d1f7
Hongsj03 <121038257+Hongsj03@users.noreply.github.com>
2023-11-19 23:07:20 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int n=0;
+
+    scanf("%d",&n);
+    
+    for(int i =0;i<n/4;i++){
+        printf("long ");
+    }
+    printf("int");
+
+    return 0;
+}
